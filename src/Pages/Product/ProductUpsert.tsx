3b7bf1a9a78800475e0bc58c2e238245f64e1595
Anton Fesenko <tohafesenkolovep@gmail.com>
2024-02-08 20:59:06 +0200

    Fix: ProductUpsert loader

--- src/Pages/Product/ProductUpsert.tsx
+++ src/Pages/Product/ProductUpsert.tsx
@@ -55,6 +55,8 @@ function ProductUpsert() {
   }, [productsData]);
 
   useEffect(() => {
+    setLoading(true);
+
     if (brandsData) {
       setBrands(brandsData.result);
     }
@@ -64,6 +66,8 @@ function ProductUpsert() {
     if (specialTagsData) {
       setSpecialTags(specialTagsData.result);
     }
+
+    setLoading(false);
   }, [brandsData, categoriesData, specialTagsData]);
 
   const handleProductInput = (
@@ -107,110 +111,120 @@ function ProductUpsert() {
   };
 
   return (
-    <div className="container border mt-5 p-5 bg-light">
+    <>
       {loading && <MainLoader />}
-      <h3 className="px-2 text-primary text-center">
-        {id ? "Edit Product" : "Add Product"}
-      </h3>
-      <form method="post" encType="multipart/form-data" onSubmit={handleSubmit}>
-        <div className="row mt-3" style={{ justifyContent: "center" }}>
-          <div className="col-md-7">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter Name"
-              required
-              name="name"
-              value={productInputs.name}
-              onChange={handleProductInput}
-            />
-
-            <select
-              className="form-control mt-3 form-select"
-              required
-              name="brandId"
-              value={productInputs.brandId}
-              onChange={handleProductInput}
+      {!loading && (
+        <>
+          <div className="container border mt-5 p-5 bg-light">
+            <h3 className="px-2 text-primary text-center">
+              {id ? "Edit Product" : "Add Product"}
+            </h3>
+            <form
+              method="post"
+              encType="multipart/form-data"
+              onSubmit={handleSubmit}
             >
-              {brands.map((brand: Brand) => (
-                <option value={brand.id}>{brand.name}</option>
-              ))}
-            </select>
+              <div className="row mt-3" style={{ justifyContent: "center" }}>
+                <div className="col-md-7">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter Name"
+                    required
+                    name="name"
+                    value={productInputs.name}
+                    onChange={handleProductInput}
+                  />
 
-            <textarea
-              className="form-control mt-3"
-              placeholder="Enter Description"
-              name="description"
-              rows={10}
-              value={productInputs.description}
-              onChange={handleProductInput}
-            />
+                  <select
+                    className="form-control mt-3 form-select"
+                    required
+                    name="brandId"
+                    value={productInputs.brandId}
+                    onChange={handleProductInput}
+                  >
+                    {brands.map((brand: Brand) => (
+                      <option value={brand.id}>{brand.name}</option>
+                    ))}
+                  </select>
 
-            <select
-              className="form-control mt-3 form-select"
-              required
-              name="specialTagId"
-              value={productInputs.specialTagId}
-              onChange={handleProductInput}
-            >
-              {specialTags.map((specialTag: SpecialTag) => (
-                <option value={specialTag.id}>{specialTag.name}</option>
-              ))}
-            </select>
+                  <textarea
+                    className="form-control mt-3"
+                    placeholder="Enter Description"
+                    name="description"
+                    rows={10}
+                    value={productInputs.description}
+                    onChange={handleProductInput}
+                  />
 
-            <select
-              className="form-control mt-3 form-select"
-              required
-              name="categoryId"
-              value={productInputs.categoryId}
-              onChange={handleProductInput}
-            >
-              {categories.map((category: Category) => (
-                <option value={category.id}>{category.name}</option>
-              ))}
-            </select>
+                  <select
+                    className="form-control mt-3 form-select"
+                    required
+                    name="specialTagId"
+                    value={productInputs.specialTagId}
+                    onChange={handleProductInput}
+                  >
+                    {specialTags.map((specialTag: SpecialTag) => (
+                      <option value={specialTag.id}>{specialTag.name}</option>
+                    ))}
+                  </select>
 
-            <input
-              type="number"
-              className="form-control mt-3"
-              placeholder="Enter Price"
-              required
-              name="price"
-              value={productInputs.price}
-              onChange={handleProductInput}
-            />
+                  <select
+                    className="form-control mt-3 form-select"
+                    required
+                    name="categoryId"
+                    value={productInputs.categoryId}
+                    onChange={handleProductInput}
+                  >
+                    {categories.map((category: Category) => (
+                      <option value={category.id}>{category.name}</option>
+                    ))}
+                  </select>
 
-            <input
-              type="text"
-              className="form-control mt-3"
-              placeholder="Enter Picture Link"
-              required
-              name="imageUrl"
-              value={productInputs.imageUrl}
-              onChange={handleProductInput}
-            />
-            <div className="row">
-              <div className="col-6">
-                <button
-                  type="submit"
-                  className="btn btn-primary form-control mt-3"
-                >
-                  {id ? "Update" : "Create"}
-                </button>
+                  <input
+                    type="number"
+                    className="form-control mt-3"
+                    placeholder="Enter Price"
+                    required
+                    name="price"
+                    value={productInputs.price}
+                    onChange={handleProductInput}
+                  />
+
+                  <input
+                    type="text"
+                    className="form-control mt-3"
+                    placeholder="Enter Picture Link"
+                    required
+                    name="imageUrl"
+                    value={productInputs.imageUrl}
+                    onChange={handleProductInput}
+                  />
+                  <div className="row">
+                    <div className="col-6">
+                      <button
+                        type="submit"
+                        className="btn btn-primary form-control mt-3"
+                      >
+                        {id ? "Update" : "Create"}
+                      </button>
+                    </div>
+                    <div className="col-6">
+                      <a
+                        onClick={() => navigate("/product/productList")}
+                        className="btn btn-secondary form-control mt-3"
+                      >
+                        Back to Menu Items
+                      </a>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="col-6">
-                <a
-                  onClick={() => navigate("/product/productList")}
-                  className="btn btn-secondary form-control mt-3"
-                >
-                  Back to Menu Items
-                </a>
-              </div>
-            </div>
+            </form>
           </div>
-        </div>
-      </form>
-    </div>
+        </>
+      )}
+    </>
   );
 }
 

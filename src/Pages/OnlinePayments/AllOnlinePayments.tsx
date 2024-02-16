e4aaa0e76f24cb9a0e8fdf635d4b02891bb288ce
Anton Fesenko <tohafesenkolovep@gmail.com>
2024-02-16 13:14:44 +0200

    Feat: Add filters to Online Payments pages

--- src/Pages/OnlinePayments/AllOnlinePayments.tsx
+++ src/Pages/OnlinePayments/AllOnlinePayments.tsx
@@ -1,8 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import { withAdminAuth } from "../../HOC";
 import { useGetAllPaymentsQuery } from "../../APIs/onlinePaymentsApi";
 import { OnlinePaymentList } from "../../Components/Page/OnlinePayments";
 import { MainLoader } from "../../Components/Page/Common";
+import { PaymentStatuses } from "../../Static";
+import { inputHelper } from "../../Helper";
+
+const filterOptions = ["All", PaymentStatuses.PAID, PaymentStatuses.RETURNED];
 
 function AllOnlinePayments() {
   const [filters, setFilters] = useState({ status: "" });
@@ -17,6 +21,19 @@ function AllOnlinePayments() {
     }),
   });
 
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const tempValue = inputHelper(e, filters);
+    setFilters(tempValue);
+  };
+
+  const handleFilters = () => {
+    setApiFilters({
+      status: filters.status,
+    });
+  };
+
   useEffect(() => {
     if (data) {
       setPaymentsData(data.apiResponse.result);
@@ -30,6 +47,25 @@ function AllOnlinePayments() {
         <>
           <div className="d-flex align-items-center justify-content-between mx-5 mt-5">
             <h1 className="text-primary">Online Payments</h1>
+            <div className="d-flex" style={{ width: "15%" }}>
+              <select
+                className="form-select mx-2"
+                onChange={handleChange}
+                name="status"
+              >
+                {filterOptions.map((item, index) => (
+                  <option key={index} value={item === "All" ? "" : item}>
+                    {item}
+                  </option>
+                ))}
+              </select>
+              <button
+                className="btn btn-outline-primary"
+                onClick={handleFilters}
+              >
+                Filter
+              </button>
+            </div>
           </div>
           <OnlinePaymentList isLoading={isLoading} paymentData={paymentsData} />
         </>
--- src/Pages/OnlinePayments/MyOnlinePayments.tsx
+++ src/Pages/OnlinePayments/MyOnlinePayments.tsx
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { withAuth } from "../../HOC";
 import { useSelector } from "react-redux";
 import { RootState } from "../../Storage/store";
 import { useGetAllPaymentsQuery } from "../../APIs/onlinePaymentsApi";
 import { MainLoader } from "../../Components/Page/Common";
 import { OnlinePaymentList } from "../../Components/Page/OnlinePayments";
+import { PaymentStatuses } from "../../Static";
+import { inputHelper } from "../../Helper";
+
+const filterOptions = ["All", PaymentStatuses.PAID, PaymentStatuses.RETURNED];
 
 function MyOnlinePayments() {
   const userId = useSelector((state: RootState) => state.userAuthStore.id);
-  const { data, isLoading } = useGetAllPaymentsQuery({ userId });
+  const [filters, setFilters] = useState({ status: "" });
+  const [paymentsData, setPaymentsData] = useState([]);
+  const [apiFilters, setApiFilters] = useState({
+    status: "",
+    userId: userId,
+  });
+
+  const { data, isLoading } = useGetAllPaymentsQuery({
+    ...(apiFilters && {
+      status: apiFilters.status,
+      userId: userId,
+    }),
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const tempValue = inputHelper(e, filters);
+    setFilters(tempValue);
+  };
+
+  const handleFilters = () => {
+    setApiFilters({
+      status: filters.status,
+      userId: userId,
+    });
+  };
+
+  useEffect(() => {
+    if (data) {
+      setPaymentsData(data?.apiResponse.result);
+    }
+  }, [data]);
 
   return (
     <>
@@ -17,6 +53,25 @@ function MyOnlinePayments() {
         <>
           <div className="d-flex align-items-center justify-content-between mx-5 mt-5">
             <h1 className="text-primary">My Online Payments</h1>
+            <div className="d-flex" style={{ width: "15%" }}>
+              <select
+                className="form-select mx-2"
+                onChange={handleChange}
+                name="status"
+              >
+                {filterOptions.map((item, index) => (
+                  <option key={index} value={item === "All" ? "" : item}>
+                    {item}
+                  </option>
+                ))}
+              </select>
+              <button
+                className="btn btn-outline-primary"
+                onClick={handleFilters}
+              >
+                Filter
+              </button>
+            </div>
           </div>
           <OnlinePaymentList
             isLoading={isLoading}
